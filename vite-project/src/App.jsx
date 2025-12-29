6c0941d0b7b01e8f2ca7f3a26c22f39e8366fb82
Anurag srivastava <anuragsrivastava8874@gmail.com>
2025-12-29 14:52:29 +0530

    DONE

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './components/navBar'
 
-function App() {
+
+const App = () => {
   return (
-    <div>App</div>
+      <NavBar/>
   )
 }
 
---
+++ vite-project/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      
+    navBar
+    
+   </div>
+  )
+}
+
+export default NavBar
